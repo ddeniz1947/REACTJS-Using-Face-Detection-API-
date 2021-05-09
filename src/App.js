a911564d6ac34d80873a6c2ec5de173db8fef697
Deniz Can DİNÇ <deniz@agencylook.com>
2021-05-09 18:54:52 +0300

    Revert "api key method added"
    This reverts commit 3b528a76d7258bcd0bb8d387859493e4a1fcac0b.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,9 @@ import Clarifai from 'clarifai';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import FooterComponent from './components/Footer/FooterComponent';
-import {returnApiKey} from './config.js';
 
 const app = new Clarifai.App({
-  apiKey: returnApiKey()
+  apiKey: '63a91100bc474995bc84608688334c82'
 });
 
 
@@ -246,9 +245,15 @@ class App extends Component {
           : (
             this.state.route === 'signin'
               ? 
+              <div> 
+                <Logo />
                 <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+              </div>
               :
+              <div> 
+                <Logo /> 
                 <Register onRouteChange={this.onRouteChange} />
+              </div>
               
           )
 
--- src/config.js
+++
@@ -1,4 +0,0 @@
-
-  export const returnApiKey = ()=>{
-    return '63a91100bc474995bc84608688334c82';
-  }
